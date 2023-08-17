42fc3885be7b684ae6351b16ca2df5de0d5d3e84
koushikdp <kdamaraju@primesoft.net>
2023-08-17 14:13:07 +0530

    this is for my maven project

---
+++ src/test/java/demo/Junit4IT.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Junit4IT {
+
+  @Test
+  public void testProperty() {
+    assertTrue(true);
+  }
+
+}
