df8275a9ad286d0ba866ce6fa59dd6b7a6a59524
koushikdp <kdamaraju@primesoft.net>
2023-08-20 17:52:04 +0530

    updated settings.xml

--- src/test/java/demo/Junit4IT.java
+++ src/test/java/demo/Junit4IT.java
@@ -8,7 +8,7 @@ public class Junit4IT {
     @Test
     public void testMultiply() {
         int result = multiply(2, 3);
-        assertEquals(5, result);
+        assertEquals(6, result);
     }
 
     private int multiply(int a, int b) {
